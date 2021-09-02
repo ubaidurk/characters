c96cd0af46fcdbec9cc4e54ee016781e5dc7ece4
ubaidurk <77473624+ubaidurk@users.noreply.github.com>
2021-09-02 11:51:52 -0400

    Update rarities.sol

--- rarities.sol
+++ rarities.sol
@@ -2,8 +2,6 @@ uint256 constant SPECIAL_RACE_CHANCE = 20;
 
 uint256 constant SUBRACE_CHANCE = 20;
 
-uint256 constant SPECIAL_CLASS_CHANCE = 40;
-
 uint256 constant SUBCLASS_CHANCE = 40;
 
 uint256 constant FACEPAINT_CHANCE = 5;
